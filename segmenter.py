7249dc6a19dc5ea4acae68f6c19aefeca34e37ae
Joshua Harris <jeshhurse@MacBook-Air-427.local>
2023-09-21 16:11:22 -0400

    My commit message

---
+++ segmenter.py
@@ -0,0 +1,9 @@
+import sys
+
+line = sys.stdin.readline()
+
+while line:
+        print(line.replace('.', '.\n'))
+        line = sys.stdin.readline()
+
+
--- wc.py
+++ wc.py
@@ -3,18 +3,27 @@ import sys
 linecount = 0
 tokencount = 0
 charactercount = 0
+vowelcount = 0
+conscount = 0
 
 datainput = sys.stdin.readlines()
 
 for line in datainput:
-        linecount += 1
+        linecount = linecount + 1
 
         for char in line:
                 charactercount += 1
                 if char == " ":
-                        tokencount += 1
+                        tokencount = tokencount + 1
+                if char in 'aeiou':
+                        vowelcount = vowelcount + 1
+                if char in 'bcdfghjklmnpqrstvwxyz':
+                        conscount = conscount + 1
 
 sys.stdout.write(str(linecount)+'\n')
 sys.stdout.write(str(tokencount)+'\n')
 sys.stdout.write(str(charactercount)+'\n')
+sys.stdout.write(str(vowelcount)+'\n')
+sys.stdout.write(str(conscount)+'\n')
+
 
